857150f75f07356258e2e25583d0443d080f2d62
kanke léon AGBEKPONOU <kankoe-leon.agbekponou@uha.fr>
2024-05-06 21:32:05 +0200

    ceation de l'API et ajout des instruction

---
+++ README.md
@@ -0,0 +1,19 @@
+#Documentation du projet
+## Pour demarrer l'API executer les commande suivantes dans le dossier courant:
+
+```bash
+php artisan migrate
+
+```
+## Executer la commande suivante pour génerer les valeur 
+```bash
+php artisan db:seed --class=CategoryProductSeeder
+```
+## Pour demarrer l'API
+```bash
+php artisan serve
+```
+## Pour tester l'API 
+```bash
+http://localhost:préciser_le_port/products
+```
---
+++ database/seeders/CategoryProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoryProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::factory()
+            ->has(Product::factory()->count(4))
+            ->count(10)
+            ->create();
+    }
+}
